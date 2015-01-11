ce8cfb17cc1ea0985fba1d82fb288c35cdefb911
Justin Self <justinself@outlook.com>
2015-01-10 22:51:59 -0600

    init

---
+++ src/ClickJacking/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClickJacking.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ClickJacked()
+        {
+            return View();
+        }
+
+        public ActionResult SafeIndex()
+        {
+            HttpContext.Response.AddHeader("X-Frame-Options", "DENY");
+            return View();
+        }
+
+        public ActionResult CannotClickJack()
+        {
+            return View();
+        }
+    }
+}
