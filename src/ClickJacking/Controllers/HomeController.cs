fd3c17c9b4399fbccd36c24c9db4bf248ab53022
Justin Self <justinself@outlook.com>
2015-01-11 00:51:16 -0600

    renamed the controller

--- src/ClickJacking/Controllers/HomeController.cs
+++ src/ClickJacking/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace ClickJacking.Controllers
 {
-    public class ClickJacking : Controller
+    public class HomeController : Controller
     {
         public ActionResult Index()
         {
